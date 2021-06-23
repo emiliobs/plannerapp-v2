821f7426308e3fcd4dddcf377d795ac1bac34b94
emilio barrera <barrera_emilio@yahoo.es>
2021-06-23 12:33:07 +0100

    Discover the V2.0 API test It with

---
+++ src/PlannerApp.Shared/Response/ApiResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace PlannerApp.Shared.Response
+{
+    public class ApiResponse
+    {
+        public string Message { get; set; }
+
+        public bool IsSuccess { get; set; }
+    }
+
+    public class ApiResponse<T> : ApiResponse
+    {
+        public T Value { get; set; }
+    }
+
+}
