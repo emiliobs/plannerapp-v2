ffa5925ed1140895de3bbc2b555fa887785df005
emilio barrera <barrera_emilio@yahoo.es>
2021-06-29 13:02:07 +0100

    In this lesson, we will create and setup the HttpClient to enable us to send requests much easier to the API by fetch the access token from the local storage and setting it up in the header of each HttpRequest

---
+++ src/PlannerApp/AuthorizationMessageHandler.cs
@@ -0,0 +1,30 @@
+﻿using Blazored.LocalStorage;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PlannerApp
+{
+    public class AuthorizationMessageHandler :   DelegatingHandler
+    {
+        private readonly ILocalStorageService _localStorage;
+
+        public AuthorizationMessageHandler( ILocalStorageService localStorage)
+        {
+            this._localStorage = localStorage;
+        }
+
+        protected async override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            if (await _localStorage.ContainKeyAsync("access_token"))
+            {
+                var token = await _localStorage.GetItemAsStringAsync("access_token");
+                request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+            }
+
+            System.Console.WriteLine("Authorization Message Handler Called!");
+
+            return await base.SendAsync(request, cancellationToken);
+        }
+    }
+}
