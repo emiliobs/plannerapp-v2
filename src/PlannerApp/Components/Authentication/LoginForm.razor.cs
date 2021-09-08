a42d4db381242f3641d263873ec3a2b902d8c2a2
emilio barrera <barrera_emilio@yahoo.es>
2021-09-08 16:14:20 +0100

    08 Blazor PlannerApp V2 | Implement the Logic of Login Form

--- src/PlannerApp/Components/Authentication/LoginForm.razor.cs
+++ src/PlannerApp/Components/Authentication/LoginForm.razor.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 using PlannerApp.Shared.Models;
+using PlannerApp.Shared.Response;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 
 namespace PlannerApp.Components.Authentication
@@ -13,11 +17,46 @@ namespace PlannerApp.Components.Authentication
          [Inject]
         public HttpClient HttpClient { get; set; }
 
+        [Inject]
+        public NavigationManager  Navigation{ get; set; }
+
+        [Inject]
+        public AuthenticationStateProvider AuthenticationStateProvider { get; set; }
+
+        [Inject]
+        public ILocalStorageService  Storage{ get; set; }
+
         public LoginRequest _model = new LoginRequest();
+
+        private bool _isBusy = false;
+
+        public string _errorMEssage = string.Empty;
 
         private async Task LoginUserAsync()
         {
-            throw new NotImplementedException();
+            _isBusy = true;
+            _errorMEssage = string.Empty;
+
+            var response = await HttpClient.PostAsJsonAsync("/api/v2/auth/login",_model);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var resul = await response.Content.ReadFromJsonAsync<ApiResponse<LoginResult>>();
+                //store it in the local storege;
+                await Storage.SetItemAsStringAsync("access_token", resul.Value.Token);
+                await Storage.SetItemAsync<DateTime>("expiry_date", resul.Value.ExpiryDate);
+
+                await AuthenticationStateProvider.GetAuthenticationStateAsync();
+
+                Navigation.NavigateTo("/");
+            }
+            else
+            {
+                var erroResult = await response.Content.ReadFromJsonAsync<ApiErrorResponse>();
+                _errorMEssage = erroResult.Message;
+            }
+
+            _isBusy = false;
         }
     }
 }
